387af5e94303848504f1394405e9457f6eca9ddd
AMIT KUMAR DEY <amit_cse43@yahoo.com>
2024-04-18 15:23:55 +0200

    Modify pyramid program

--- Fexercice_piramide.py
+++ Fexercice_piramide.py
@@ -3,9 +3,9 @@ I=int(input("enter level of the pyramid:"))
 
 def pyramid(I):
     for i in range(I):
-        print(' ' * (I - i - 1) + '*' * (2 * i + 1))
+        print(' ' * (I - i - 1) + '*' * (2*i + 1))
 
-10
+
 pyramid(I)
 
     
